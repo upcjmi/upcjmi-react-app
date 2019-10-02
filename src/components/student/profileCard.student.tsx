65fed836281a2ae42e4b9194efce09300747d433
Faisal Manzer <faisal_manzer@yahoo.in>
2019-10-02 06:23:31 +0530

    Updated all contributors (#21)
    * Added delay in verify email
    Added grading system to ExtraDetails
    
    * Fixed issue with course display name
    Corrected manifest.json start URL
    Bumped version to v0.3.5
    
    * Changed script to lint-staged
    Updated all collaborators
    Fixed GetProfileIcon

--- src/components/student/profileCard.student.tsx
+++ src/components/student/profileCard.student.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useEffect} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import {Avatar, Button, Card, Icon, Tag, Typography, Skeleton} from 'antd';
 
 import {selectScreen} from 'helpers/screen.helper';
@@ -7,12 +7,12 @@ import {IReduxState} from 'reducers';
 import {IStudentExtraDetails} from 'types/student.api.type';
 import {loadStudentExtraDetails} from 'actions/student.action';
 import {IUserMeta} from 'types/api.type';
+import SideDrawerForm from 'components/sideDrawerForm';
+import {getCourseName} from 'helpers/courses';
 import {GetProfileIcon} from 'helpers/function.helper';
-import SideDrawerForm from '../sideDrawerForm';
 import ExtraDetailsChange from './extraDetailsChange.student';
 import {GetProfileIcon} from 'helpers/function.helper';
 import {getCourseName} from 'helpers/courses';
-
 
 interface IStateProps {
   extraDetails: IStudentExtraDetails | undefined | null;
@@ -20,11 +20,11 @@ interface IStateProps {
 }
 
 interface IDispatchProps {
-  loadExtraDetails: any
+  loadExtraDetails: any;
 }
 
 interface IProps extends IStateProps, IDispatchProps {
-  editable: boolean
+  editable: boolean;
 }
 
 const {Title, Text, Paragraph} = Typography;
@@ -34,69 +34,65 @@ const getYear = (year: number): string => {
     case 1:
       return '1st';
     case 2:
-      return  '2nd';
+      return '2nd';
     case 3:
       return '3rd';
     default:
-      return `${year}th`
+      return `${year}th`;
   }
 };
 
+const ProfileCardStudent: FC<IProps> = ({
+  extraDetails,
+  loadExtraDetails,
+  user,
+  editable = false,
+}: IProps) => {
+  useEffect(() => {
+    loadExtraDetails();
+  }, [loadExtraDetails]);
 
-const ProfileCardStudent: FC<IProps> =
-  ({extraDetails, loadExtraDetails, user, editable=false}: IProps) => {
-
-    useEffect(() => {
-      loadExtraDetails();
-    }, [loadExtraDetails]);
-
-    if(extraDetails === undefined || user === undefined)
-      return (
-        <Card>
-          <Skeleton />
-        </Card>
-      );
-
-    if(extraDetails === null){
-      return (
-        <Card>
-          <Title level={4}>You have not added your extra info yet</Title>
-          <SideDrawerForm
-
-            render={() => (
-            // @ts-ignore
-              <ExtraDetailsChange action='add' />
-            )}
-              >
-            <Button type='primary' size='large'>
-                  Click to add
-            </Button>
-          </SideDrawerForm>
-        </Card>
-      );
-    }
-
+  if (extraDetails === undefined || user === undefined)
     return (
       <Card>
-        <div style={{display: 'flex'}}>
-          <div style={{
+        <Skeleton />
+      </Card>
+    );
+
+  if (extraDetails === null) {
+    return (
+      <Card>
+        <Title level={4}>You have not added your extra info yet</Title>
+        <SideDrawerForm
+          render={() => (
+            // @ts-ignore
+            <ExtraDetailsChange action='add' />
+          )}>
+          <Button type='primary' size='large'>
+            Click to add
+          </Button>
+        </SideDrawerForm>
+      </Card>
+    );
+  }
+
+  return (
+    <Card>
+      <div style={{display: 'flex'}}>
+        <div
+          style={{
             display: 'flex',
             flexWrap: 'wrap',
             textAlign: selectScreen('center', null),
-            justifyContent: selectScreen('space-around', null)
+            justifyContent: selectScreen('space-around', null),
           }}>
-
-            <Avatar icon='user' size={64} />
-            <div style={{
-              marginLeft: selectScreen(0, 5, 10)
+          <Avatar icon='user' size={64} />
+          <div
+            style={{
+              marginLeft: selectScreen(0, 5, 10),
             }}>
-              <Title style={{marginBottom: 0}}>
-                {user.name}
-              </Title>
-              <Text strong>
-                {extraDetails.tag_line}
-              </Text>
-            </div>
+            <Title style={{marginBottom: 0}}>{user.name}</Title>
+            <Text strong>{extraDetails.tag_line}</Text>
           </div>
         </div>
         <br />
@@ -104,66 +100,59 @@ const ProfileCardStudent: FC<IProps> =
           <b>
             {getCourseName(extraDetails.student.course)}
           &nbsp;&nbsp;&middot;&nbsp;&nbsp;
-            {`${getYear(extraDetails.student.year)} year`}
-          </b>
-        </Text>
-        <Paragraph type='secondary'>
-          {extraDetails.about}
-        </Paragraph>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            position: 'relative',
-            left: -15
-          }}
-      >
-          {extraDetails.profiles.map(profile => (
-            <a
-              href={profile.link}
-              style={{fontSize: 30, color: '#666',}}
-              rel='noopener noreferrer'
-              target='_blank'
-          >
-              <GetProfileIcon type={profile.type} />
-            </a>
-          ))}
-        </div>
-        <div>
+          {`${getYear(extraDetails.student.year)} year`}
+        </b>
+      </Text>
+      <Paragraph type='secondary'>{extraDetails.about}</Paragraph>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          position: 'relative',
+          left: -15,
+        }}>
+        {extraDetails.profiles.map(profile => (
+          <a
+            href={profile.link}
+            style={{fontSize: 30, color: '#666'}}
+            rel='noopener noreferrer'
+            target='_blank'>
+            <GetProfileIcon type={profile.type} />
+          </a>
+        ))}
+      </div>
+      <div>
         Skills:&nbsp;&nbsp;
-          {extraDetails.skills.map(skill => (
-            <Tag>
-              {skill}
-            </Tag>
-          ))}
-        </div>
-        {
-          editable? (
-            <SideDrawerForm
-              render={() => (
-                // @ts-ignore
-                <ExtraDetailsChange action='edit' />
-              )}
-            >
-              <Button style={{float: 'right'}} type='link'>
-                <Icon type='edit' />
-                Edit
-              </Button>
-            </SideDrawerForm>
-          ) : null
-        }
-      </Card>
-    );
-  };
-
+        {extraDetails.skills.map(skill => (
+          <Tag>{skill}</Tag>
+        ))}
+      </div>
+      {editable ? (
+        <SideDrawerForm
+          render={() => (
+            // @ts-ignore
+            <ExtraDetailsChange action='edit' />
+          )}>
+          <Button style={{float: 'right'}} type='link'>
+            <Icon type='edit' />
+            Edit
+          </Button>
+        </SideDrawerForm>
+      ) : null}
+    </Card>
+  );
+};
 
 const mapStateToProps = (state: IReduxState): IStateProps => ({
   extraDetails: state.student.extraDetails,
-  user: state.auth.user
+  user: state.auth.user,
 });
 
 const mapDispatchToProps = (dispatch: any): IDispatchProps => ({
-  loadExtraDetails: () => dispatch(loadStudentExtraDetails())
+  loadExtraDetails: () => dispatch(loadStudentExtraDetails()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileCardStudent);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProfileCardStudent);
