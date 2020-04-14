f63b6ca0fea73e263860f37ab10029c509b208b0
alhaqhassan <alhaqhassan@gmail.com>
2020-04-14 23:00:00 +0530

    edit company details added

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,15 +2,18 @@ import {combineReducers} from 'redux';
 import {auth, IAuthState} from './auth.reducer';
 import {navigator, INavigatorState} from './navigator.reducer';
 import {student, IStudentState} from './student.reducer';
+import {company, ICompanyState} from './company.reducer';
 
 export interface IReduxState {
   auth: IAuthState;
   navigator: INavigatorState;
   student: IStudentState;
+  company: ICompanyState;
 }
 
 export default combineReducers({
   auth,
   navigator,
-  student
+  student,
+  company,
 });
