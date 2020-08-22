f9abf218d3b46c3bcf4401c425cfd971ac3cc880
ashertoufeeq <ashertoufeeq@gmail.com>
2020-08-22 17:38:23 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.299c3dde88cc129402fb5276bf5f842e.js"
+  "/precache-manifest.544cd85469e440d2fe770a13e766b5c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- static/js/48.b5aa82f1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpupcjmi=this.webpackJsonpupcjmi||[]).push([[48],{843:function(e,t,n){"use strict";n.r(t);n(115);var a=n(59),i=n(0),s=n.n(i),c=a.a.Title;t.default=function(){return s.a.createElement("div",{className:"container"},s.a.createElement(c,null,"Terms and Condition"),"This is yet to be updated")}}}]);
-//# sourceMappingURL=48.b5aa82f1.chunk.js.map
