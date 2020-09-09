0c653757791e2a1bdd498606083708d68f47818c
ashertoufeeq <ashertoufeeq@gmail.com>
2020-09-10 02:43:23 +0530

    Littile chnges

--- src/components/CoordinatorCard.tsx
+++ src/components/CoordinatorCard.tsx
@@ -1,34 +1,29 @@
-import {Card, Icon, Tooltip,Typography} from 'antd';
+import {Card, Icon, Avatar,Tooltip,Typography} from 'antd';
 import React from 'react';
 import {ICoordinator} from '../types/common.type';
 import {getCourseName} from '../helpers/courses';
 
 const {Title} = Typography
+const {Meta} = Card;
 
 export const CoordinatorCard = ({department,email,name,phone,photo,type}:ICoordinator) =>{
   const desc = type==='S'?'Student Coordinator':'Teacher Coordinator';
   return (
     <Card
       style={{ width: 300,margin:10 }}
-      cover={(
-        <img
-          alt={name}
-          src={photo}
-        />
-      )}
     >
-      <Title level={4}>{name}</Title>
-      <p className='m-0'>{desc}</p>
-      <p className='m-0'>{getCourseName(department)}</p>
-      <br />
-      <div className='row'>
-        <Tooltip placement='topLeft' trigger='click' title={email}>
-          <Icon type='mail' key='mail' style={{fontSize:20}} />
-        </Tooltip>
-        <Tooltip placement='topLeft' trigger='click' title={phone}>
-          <Icon type='phone' key='phone' className='mx' style={{fontSize:20}} />
-        </Tooltip>
-      </div>
+      <Meta
+        avatar={<Avatar src={photo} />}
+        title={<Title level={4}>{name}</Title>}
+        description={(
+          <div>
+            <p className='m-0'>{desc}</p>
+            <p className='m-0'>{getCourseName(department)}</p>
+            <p className='m-0'>{phone}</p>
+            <p className='m-0'>{email}</p>
+          </div>
+)}
+      />
     </Card>
   )
 }
