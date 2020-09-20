bfd91fc3df4e51bdfd477a6465a08bb5aa5e2bcd
ashertoufeeq <ashertoufeeq@gmail.com>
2020-09-20 15:47:13 +0530

    Ui Changes

--- src/screens/home.screen.tsx
+++ src/screens/home.screen.tsx
@@ -1,10 +1,10 @@
 import React, {FC, useEffect, useState} from 'react';
+import Slider from "react-slick";
 import {Skeleton, Tag, Typography, Card,Row,Col ,Carousel} from 'antd';
 import OverlayCarousel from 'components/overlayCarousel';
 import AboutUs from 'components/home/aboutUs';
 import {HOME_CAROUSEL} from 'constants/home/carousel.home.constant';
 import TopRecruiters from 'components/home/topRecruiters';
-import {selectScreen} from 'helpers/screen.helper';
 import {
   COORDINATORS,
   PORTAL_HOME_PATH,
@@ -13,24 +13,19 @@ import {
 import {Link} from 'react-router-dom';
 import Notice from 'components/home/notice';
 import { getBadges} from 'helpers/api/core.api.helper';
-import {CardRanking} from 'components/cardRanking';
 import {IRanking} from 'types/common.type';
+import {BADGES} from 'constants/home/badges.home.constant';
 import ContactScreen from './contact.screen';
+import {TOP_RECRUITERS} from '../constants/home/topRecruiters.home.constant';
+import {selectScreen} from '../helpers/screen.helper';
+
+
+const JamiaLogo = require('assets/svgs/jamia-logo-white.svg');
 
 interface IProps {}
 
 const {Title} = Typography;
-// const dummy = {
-//   link:'https://facebook.com',
-//   id:1,
-//   issuer:'Outlook ranking',
-//   rank:'1st',
-//   year:2020,
-//   heading:'all around the world among top 50 universities.',
-//   show_on_site:true,
-//   created:'kajhsdf',
-//   modified:'aksdhjf',
-// }
+
 
 const quickLink = (
   <div className='space-child'>
@@ -55,6 +50,7 @@ const quickLink = (
     </Link>
   </div>
 );
+
 const getBadgesArray= (arr:Array<any>) => {
   const newArr = [];
   for(let i = 0; i<arr.length;i+=4){
@@ -62,10 +58,69 @@ const getBadgesArray= (arr:Array<any>) => {
   }
   return newArr
 }
+const Arrows = () =>{
+  return(<div />)
+}
+
+const TopRecruitersComp=()=>(
+  <Slider {...{
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
+    nextArrow: <Arrows />,
+    prevArrow: <Arrows />
+  }}>
+    {TOP_RECRUITERS.map((recruiter, index) => (
+      <Col
+        xs={24}
+        md={8}
+        className='center margin-bottom-20 margin-top-20'
+        key={index.toString()}>
+        <div className='row justify-center'>
+          <img src={recruiter} alt='' className='image-top-recruiter' />
+        </div>
+      </Col>
+    ))}
+  </Slider>
+)
 
 const CarouselOverlayComponent = () => {
   const [apiBadges,setApiBadges] = useState<Array<IRanking>>([])
   const [loading,setLoading] = useState(false)
+  const [badgeIndex,setBadgeIndex] = useState(0);
+  const [invertedClass,setInvertedClass] = useState(false);
+
   useEffect(()=>{
     const getData  =  async ()=>{
       setLoading(true);
@@ -75,68 +130,61 @@ const CarouselOverlayComponent = () => {
     }
     getData()
   },[])
+  useEffect(()=>{
+    setTimeout(()=>{
+      setInvertedClass(true)
+    },6000)
+    setTimeout(()=>{
+      if(badgeIndex+1 === BADGES.length){
+        setBadgeIndex(0)
+        setInvertedClass(false)
+      }
+      else{
+        setBadgeIndex(badgeIndex+1)
+        setInvertedClass(false)
+      }
+    },10000);
+
+  },[badgeIndex])
   return(
-    <div className='column space-between' style={{height:'100%'}}>
+    <div className='column space-between h-100'>
       <div className='column'>
         {quickLink}
-        <div className='welcome-text'>
-          <Title level={2}>Welcome to,</Title>
-          <Title>
-            University Placement Cell
-            <br />
-            <p>Jamia Millia Islamia</p>
-          </Title>
-        </div>
+        <Row className='welcome-text'>
+          <Col md={16} sm={24}>
+            <Row className='row align-center'>
+              <Col sm={24} md={4}>
+                <img
+                  src={JamiaLogo}
+                  alt='University Placement Cell, Logo'
+                  className='jmi-logo-overlay' />
+              </Col>
+              <Col sm={24} md={20}>
+                <br />
+                <Title level={2}>Welcome to,</Title>
+                <Title>
+                      University Placement Cell
+                  <br />
+                  <p>Jamia Millia Islamia</p>
+                </Title>
+              </Col>
+            </Row>
+          </Col>
+          <Col sm={24} md={8}>
+            <img
+              src={BADGES[badgeIndex].badge}
+              alt='University Placement Cell, Logo'
+              className={`${invertedClass?'rank-badge-inverted':'rank-badge'}`} />
+          </Col>
+        </Row>
       </div>
-
-      <div className='ranking-container'>
-        <Carousel autoplay dots={false}>
-          {getBadgesArray(apiBadges).map((item,index) => (
-            <div className='row-container'>
-              <Row gutter={32} style={{display: selectScreen('none', 'none', null)}}>
-                {
-                  item.map((badge)=>(
-                    <Col span={6}>
-                      <CardRanking {...badge} />
-                    </Col>
-                  ))
-                }
-              </Row>
-            </div>
-          ))}
-          {/* {[1,2].map((item,index) => ( */}
-          {/*  <div className='row-container'> */}
-          {/*    <Row */}
-          {/*      gutter={32} */}
-          {/*      style={{display: selectScreen('none', 'none', null)}} */}
-          {/*      type='flex' */}
-          {/*      justify='space-around' */}
-          {/*    > */}
-          {/*      { */}
-          {/*          [1,2,3,4].map((badge)=>( */}
-          {/*            <Col span={6}> */}
-          {/*              <CardRanking */}
-          {/*                {...dummy} */}
-          {/*              /> */}
-          {/*            </Col> */}
-          {/*          )) */}
-          {/*        } */}
-          {/*    </Row> */}
-          {/*  </div> */}
-          {/* ))} */}
-        </Carousel>
-      </div>
-      {loading?(
-        <div className='badge-container' style={{display: selectScreen('none', 'none', null)}}>
-          {
-            [1, 2, 3, 4].map((i) => (
-              <Card style={{ width: '16rem',margin:10 ,borderRadius:'2rem'}}>
-                <Skeleton active={loading} key={i.toString()}  />
-              </Card>
-            ))
-          }
-        </div>
-      ):null}
+      <Row
+        className='white'
+        style={{display:selectScreen('none','none',null,null,null,null)}}>
+        <Col md={24}>
+          <TopRecruitersComp />
+        </Col>
+      </Row>
     </div>
   );
 }
@@ -150,9 +198,17 @@ const HomeScreen: FC<IProps> = () =>
         images={HOME_CAROUSEL}
         carousel={{dotPosition: 'bottom'}}
       />
+      <Row
+        className='white'
+        style={{display:selectScreen(null,null,'none','none','none','none')}}
+      >
+        <Col md={24}>
+          <TopRecruitersComp />
+        </Col>
+      </Row>
       <Notice />
       <AboutUs />
-      <TopRecruiters />
+      {/* <TopRecruiters /> */}
       <ContactScreen />
     </>
   );}
